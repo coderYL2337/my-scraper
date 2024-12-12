ffc75159353b5943d61bf717450bbc66c2dac721
coderYL2337 <coderyl2337@outlook.com>
2024-12-12 02:04:14 -0500

    initial commit

---
+++ src/app/api/scraper/route.ts
@@ -0,0 +1,32 @@
+import chromium from "@sparticuz/chromium-min";
+import puppeteer from "puppeteer-core";
+
+chromium.setHeadlessMode = true;
+
+chromium.setGraphicsMode = false;
+
+export async function POST() {
+  // Optional: Load any fonts you need. Open Sans is included by default in AWS Lambda instances
+  await chromium.font(
+    "https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf"
+  );
+  const isLocal = !!process.env.CHROME_EXECUTABLE_PATH;
+
+  const browser = await puppeteer.launch({
+    args: isLocal
+      ? puppeteer.defaultArgs()
+      : [...chromium.args, "--hide-scrollbars", " --incognito", "--no-sandbox"],
+    defaultViewport: chromium.defaultViewport,
+    executablePath:
+      process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath()),
+    headless: chromium.headless,
+  });
+
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  const pageTitle = await page.title();
+  await browser.close();
+  return Response.json({
+    test: true,
+  });
+}
